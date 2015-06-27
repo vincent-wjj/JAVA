fb24af84a3400db6bac95e104d14cc41e571a4ea
vincent-wjj <wjj19890503@gmail.com>
2015-06-27 21:46:23 +0800

    add a second line

--- src/com/vincent/test/HelloWorld.java
+++ src/com/vincent/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("add a new line!");
+		System.out.println("add a second new line!");
 
 	}
 
